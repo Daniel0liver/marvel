e24c14f3236b3d4bc66a15d0ed10141506b57f20
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-14 23:35:14 -0300

    chore: create project with create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <main>
+        <code>Time to work!</code>
+      </main>
+    </div>
+  );
+};
+
+export default App;
