b89a812f5e5d9ee812baff01cfb43717e8b40e15
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-16 00:24:36 -0300

    chore: add react lazy to code splitting and add react suspense to wait for component code to load

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,29 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Characters from 'pages/Characters';
+const Characters = lazy(() => import('pages/Characters'));
 
 const Routes = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Characters} />
-      </Switch>
+      <Suspense fallback={<div>loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Characters} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
 
 export default Routes;
+
+/*
+  React lazy: é um code split, permite carregar apenas o necessario, ou seja, não
+  carrega todo os components de uma vez, mas apenas quando for requisitado ou 
+  quando for necessário o uso desse componente.
+*/
+
+/*
+  React Suspense: Permite esperar o carregamento de um código e enquanto o código
+  carrega, retorna um fallback que pode ser um componente react para indicar ação de loading.
+*/
