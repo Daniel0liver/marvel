c0ca70afb734294ead63191430256926801597b6
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-20 20:20:59 -0300

    chore: typing return api

--- src/hooks/fetchApi.ts
+++ src/hooks/fetchApi.ts
@@ -2,9 +2,25 @@ import { useCallback, useEffect, useState } from 'react';
 
 import Api from 'api/core';
 
+interface DataState {
+  limit: number;
+  offset: number;
+  count: number;
+  total: number;
+  results: Array<{
+    id: number;
+    name: string;
+    description: string;
+    thumbnail: {
+      extension: string;
+      path: string;
+    };
+  }>;
+}
+
 const useFetchApi = (resource: string) => {
   const [isFetching, setIsFetching] = useState(true);
-  const [data, setData] = useState<object | null>(null);
+  const [data, setData] = useState<DataState | null>(null);
   const [error, setError] = useState<object | null>(null);
 
   const fetchData = useCallback(async () => {
