7490eeb17fd7848b6c95b25194b0026ae5a980dc
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-19 22:16:03 -0300

    chore: typing process.env

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    REACT_APP_MARVEL_API: string;
+    REACT_APP_MARVEL_PUBLIC_KEY: string;
+    REACT_APP_MARVEL_PRIVATE_KEY: string;
+  }
+}
