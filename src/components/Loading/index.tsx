920749f8bcc8dafd2d672defcc5360a0bf51b51e
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-20 21:54:15 -0300

    feat: create a loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+const Loading = () => {
+  return (
+    <div id="wrapper">
+      <div className="bulletouter">
+        <div className="bulletinner"></div>
+        <div className="mask"></div>
+        <div className="dot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
