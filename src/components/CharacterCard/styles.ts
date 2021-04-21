3a05308f2bcc39ae8069db7ac8434ce6fdb61ca9
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-04-20 21:06:59 -0300

    chore: add image hover effect

--- src/components/CharacterCard/styles.ts
+++ src/components/CharacterCard/styles.ts
@@ -2,9 +2,19 @@ import styled from 'styled-components';
 
 export const StyledCard = styled.div`
   position: relative;
-  width: 300px;
-  height: auto;
+  width: 270px;
+  height: 300px;
   margin: 20px;
+  border-radius: 10px;
+  background-color: #1f1f23;
+  overflow: hidden;
+
+  &:hover {
+    img {
+      transform: scale(1.1);
+      cursor: pointer;
+    }
+  }
 `;
 
 export const StyledHeader = styled.figure``;
@@ -15,12 +25,12 @@ export const StyledFooter = styled.div`
   align-items: center;
 
   width: 300px;
-  height: 60px;
+  height: 50px;
 
   position: absolute;
   bottom: 0;
   padding-left: 1rem;
-  border-left: solid 4px #e62429;
+  border-left: solid 5px #e62429;
 
   background-color: rgba(14, 14, 16, 0.8);
   color: #fff;
@@ -28,12 +38,12 @@ export const StyledFooter = styled.div`
 
 export const StyledImage = styled.img`
   display: block;
-  margin: 0;
   object-fit: cover;
   object-position: center center;
-  padding: 0;
   width: 100%;
-  height: 100%;
+  height: 300px;
+  transform: scale(1);
+  transition: all 0.3s linear;
 `;
 
 export const StyledH3 = styled.h3`
